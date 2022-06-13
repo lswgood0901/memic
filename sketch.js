801f31abc4e6c40391caf74ee7724e97fe40a18a
lswgood0901 <lswgood0901@gmail.com>
2022-06-13 23:32:33 +0900

    toggle

--- sketch.js
+++ sketch.js
@@ -1,3 +1,6 @@
+let img_list = [];
+let button_list = []
+let mouse_event;
 let img;
 let facemesh_input;
 let predictions_input = [];
@@ -9,14 +12,98 @@ let poseNet_video;
 let poseNet_video_poses = [];
 let sillhoutte_distance = [];
 let video;
+let input_flag = false;
+let pose_flag = false;
 let button_flag = false;
 let button;
+let temp_i;
 
+function preload() {
+  for (let i = 1; i < 12; i++){
+    let list = 'images/' + i + '.jpg'
+    let img1 = loadImage(list)
+    img_list.push(img1)
+  }
+}
 function setup() {
-  createCanvas(480, 640);
+  createCanvas(480, 1600)
+  for (let i = 0; i < 11; i++) {
+    button_list.push(new Button(i, img_list[i]))
+    
+    // button_list[i].display()
+    // button_list[i].over()
+    // image(img_list[i], 8+(i)%2*228+i%2*8, 16+Math.floor(i/2)*228+i%2*8, 228, 228)
+  }
+  // createCanvas(480, 640);
+  // img = createImg('images/lisa_2.jpg', imageReady);
+  // img.size(480, 480);
+  // img.hide(); 
+  
+  // video = createCapture(VIDEO);
+  // video.size(480, 480);
+  
+  // poseNet_video = ml5.poseNet(video);
+  // poseNet_video.on('pose', function(results) {
+  //   poseNet_video_poses = results;
+  // });
+  // facemesh_video = ml5.facemesh(video);
+  // facemesh_video.on("predict", results => {
+  //   predictions_video =  results;
+  // });
+  // video.hide()
+  // button = createButton("click me")
+  // button.position(220, 600)
+  // button.mousePressed(() => {
+  //   button_flag = !button_flag
+  //   console.log(button_flag)
+  // })
+}
 
-  img = createImg('images/lisa_2.jpg', imageReady);
-  // img.size(384, 480);
+function draw() {
+  temp_i = undefined
+  if(!input_flag){
+    for (let i = 0; i < 11; i++) {
+      button_list[i].display()
+      if (button_list[i].over()) {
+        temp_i = i
+      }
+      // image(img_list[i], 8+(i)%2*228+i%2*8, 16+Math.floor(i/2)*228+i%2*8, 228, 228)
+    }
+  }
+  if (pose_flag) {
+    mode_transition(temp_i)
+    pose_flag = false
+    input_flag = true
+  }
+  if(input_flag){
+    background(255)
+    image(video, 0, 0, 640, 480);
+    drawInput(button_flag, predictions_input, poseNet_input_poses)
+    drawSilhouette(button_flag, predictions_video, predictions_input, poseNet_video_poses, poseNet_input_poses)
+    }
+}
+
+function imageReady(){
+  poseNet_input = ml5.poseNet(posemodelReady);
+  poseNet_input.on('pose', function (results) {
+      poseNet_input_poses = results;
+  });
+  facemesh_input = ml5.facemesh(facemodelReady);
+  facemesh_input.on("predict", results => {
+    predictions_input = results;
+  });
+}
+function facemodelReady() {
+  console.log("Face Model ready!");
+  facemesh_input.predict(img);
+}
+function posemodelReady() {
+  console.log("Pose Model ready!");
+  poseNet_input.singlePose(img)
+}
+function mode_transition(i) {
+  resizeCanvas(480, 640);
+  img = createImg('images/'+(i+1)+'.jpg', imageReady);
   img.size(480, 480);
   img.hide(); 
   
@@ -35,168 +122,139 @@ function setup() {
   button = createButton("click me")
   button.position(220, 600)
   button.mousePressed(() => {
-    console.log("hi")
+  button_flag = !button_flag
   })
 }
-
-function draw() {
-  background(255)
-  image(video, 0, 0, 640, 480);
-  drawInput(true, predictions_input, poseNet_input_poses)
-  drawSilhouette(true, predictions_video, predictions_input, poseNet_video_poses, poseNet_input_poses)
-}
-
-function imageReady(){
-
-  poseNet_input = ml5.poseNet(posemodelReady);
-  poseNet_input.on('pose', function (results) {
-      poseNet_input_poses = results;
-  });
-  facemesh_input = ml5.facemesh(facemodelReady);
-  facemesh_input.on("predict", results => {
-    predictions_input = results;
-  });
-}
-function facemodelReady() {
-  console.log("Face Model ready!");
-  facemesh_input.predict(img);
-}
-function posemodelReady() {
-  console.log("Pose Model ready!");
-  poseNet_input.singlePose(img)
-}
-
 function drawInput(toggle, prediction, poses) {
-  for (let i = 0; i < prediction.length; i += 1) {
-    let flag = prediction[i].scaledMesh ?? false
-    if (flag) {
-      
-    }
-    const keypoints = prediction[i].scaledMesh
-    for (let j = 0; j < keypoints.length; j += 1) {
-      const [x1, y1] = keypoints[j];
-      noStroke()
-      fill("rgba(230, 230, 230, 0.8)")
-      ellipse(x1,y1,3,3)
-    }
-  }
-  for (let i = 0; i < poses.length; i++) {
-    let pose = poses[i].pose
-    let skeleton = poses[i].skeleton
-    for (let j = 0; j < pose.keypoints.length; j++) {
-        let keypoint = pose.keypoints[j];
-        if (keypoint.score >  0.1 && j > 4) {
-          fill(230, 230, 230);
-          noStroke();
-          ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-          textSize(12)
-          text(keypoint.part, keypoint.position.x+8, keypoint.position.y+8)
-        }
-    }
-    for (let j = 0; j < skeleton.length; j++) {
-        let partA = skeleton[j][0];
-        let partB = skeleton[j][1];
-        stroke(255);
-        strokeWeight(3);
-        line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+  if(toggle){
+    for (let i = 0; i < prediction.length; i += 1) {
+      let flag = prediction[i].scaledMesh ?? false
+      if (flag) {
+        
       }
-  }
-}
-function drawSilhouette(videoFlag, prediction1, prediction2, pose1, pose2) {
-  for (let i = 0; i < prediction1.length; i++) {
-    const flag = prediction2[i] ?? false;
-    if (flag) {
-      const keypoints1 = prediction1[i].scaledMesh;
-      const keypoints2 = prediction2[i].scaledMesh;
-      for (let j = 0; j < keypoints1.length; j++) {
-        const [x1, y1] = keypoints1[j];
-        const [x2, y2] = keypoints2[j];
-        const sil_dist = dist(x1, y1, x2, y2)
-        if (sil_dist < 20) {
-          stroke("rgba(12,236,221,0.1)")
-          strokeWeight(1)
-          line(x1,y1,x2,y2)
-          noStroke()
-          fill("rgba(12, 236, 221, 0.9)")
-          ellipse(x1, y1, 4, 4)
-        }
-        else {
-          stroke("rgba(255,103,231,0.1)")
-          strokeWeight(1)
-          line(x1,y1,x2,y2)
-          noStroke();
-          fill("rgba(255, 103, 231,0.9)")
-          ellipse(x1,y1,4,4)
-        }
+      const keypoints = prediction[i].scaledMesh
+      for (let j = 0; j < keypoints.length; j += 1) {
+        const [x1, y1] = keypoints[j];
+        noStroke()
+        fill("rgba(230, 230, 230, 0.8)")
+        ellipse(x1,y1,3,3)
       }
     }
-  }
-  const pose1_flag = pose1[0] ?? false;
-  const pose2_flag = pose2[0] ?? false;
-
-  if (pose1_flag && pose2_flag) {
-    const poses1 = pose1[0].pose
-    const skeleton1 = pose1[0].skeleton
-    const poses2 = pose2[0].pose
-    const skeleton2 = pose2[0].skeleton
-    for (let i = 0; i < poses2.keypoints.length; i++) {
-      let keypoint1 = poses1.keypoints[i]
-      console.log(keypoint1)
-      let keypoint2 = poses2.keypoints[i]
-      const key_dist = dist(keypoint1.position.x, keypoint1.position.y, keypoint2.position.x, keypoint2.position.y)
-      if(keypoint1.score >  0.1 && keypoint2.score > 0.1 && i > 4){
-        if (key_dist < 20) {
-          noStroke()
-          fill("rgba(12, 236, 221, 0.9)")
-          ellipse(keypoint1.position.x, keypoint1.position.y, 10, 10)
-          textSize(12)
-          text(keypoint1.part, keypoint1.position.x+8, keypoint1.position.y+8)
-        } else {
-          noStroke()
-          fill("rgba(255, 103, 231,0.9)")
-          ellipse(keypoint1.position.x, keypoint1.position.y, 10, 10)
-          textSize(12)
-          text(keypoint1.part, keypoint1.position.x+8, keypoint1.position.y+8)
-        }
-      }
-    }
-  }
-  
-  // for (let i = 0; i < pose1.length; i++) {
-  //   const pose = pose1[i].pose;
-  //   const skeleton = pose1[i].skeleton;
-  //   const pose_distance = dist()
-  //   for (let j = 0; j < skeleton.length; j++){
-  //     let partA = skeleton[j][0]
-  //     let partB = skeleton[j][1]
-  //   }
-  // }
-}
-
-function drawPoseNet(poses) {
     for (let i = 0; i < poses.length; i++) {
-      let pose = poses[i].pose;
-      let skeleton = poses[i].skeleton;
-      for (let j = 0; j < skeleton.length; j++) {
-        let partA = skeleton[j][0];
-        let partB = skeleton[j][1];
-        fill("rgba(255, 0, 0,0.8)");
-        strokeWeight(1);
-        line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-      }
+      let pose = poses[i].pose
+      let skeleton = poses[i].skeleton
       for (let j = 0; j < pose.keypoints.length; j++) {
-        let keypoint = pose.keypoints[j];
-        if (keypoint.score >  0.1 && j > 4) {
-          fill("rgba(255, 0, 0,0.9)");
-          noStroke();
-          ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-          console.log(keypoint.part)
-          
-        }
+          let keypoint = pose.keypoints[j];
+          if (keypoint.score >  0.1 && j > 4) {
+            fill(230, 230, 230);
+            noStroke();
+            ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+            textSize(12)
+            text(keypoint.part, keypoint.position.x+8, keypoint.position.y+8)
+          }
       }
+      // for (let j = 0; j < skeleton.length; j++) {
+      //     let partA = skeleton[j][0];
+      //     let partB = skeleton[j][1];
+      //     stroke(255);
+      //     strokeWeight(3);
+      //     line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+      //   }
+    }
   }
 }
-// 실루엣 켜고 끄는 토글 버튼 추가
-// 가까이 가면 ( 포즈가 매치 되면) feedback alert 창
-// 노선 (컨셉) >> 힙한 방향으로
-// 화면 동작 
+function drawSilhouette(toggle, prediction1, prediction2, pose1, pose2) {
+  if(toggle) {
+    for (let i = 0; i < prediction1.length; i++) {
+      const flag = prediction2[i] ?? false;
+      if (flag) {
+        const keypoints1 = prediction1[i].scaledMesh;
+        const keypoints2 = prediction2[i].scaledMesh;
+        for (let j = 0; j < keypoints1.length; j++) {
+          const [x1, y1] = keypoints1[j];
+          const [x2, y2] = keypoints2[j];
+          const sil_dist = dist(x1, y1, x2, y2)
+          if (sil_dist < 20) {
+            stroke("rgba(12,236,221,0.1)")
+            strokeWeight(1)
+            line(x1,y1,x2,y2)
+            noStroke()
+            fill("rgba(12, 236, 221, 0.9)")
+            ellipse(x1, y1, 4, 4)
+          }
+          else {
+            stroke("rgba(255,103,231,0.1)")
+            strokeWeight(1)
+            line(x1,y1,x2,y2)
+            noStroke();
+            fill("rgba(255, 103, 231,0.9)")
+            ellipse(x1,y1,4,4)
+          }
+        }
+      }
+    }
+  
+    const pose1_flag = pose1[0] ?? false;
+    const pose2_flag = pose2[0] ?? false;
+
+    if (pose1_flag && pose2_flag) {
+      const poses1 = pose1[0].pose
+      const skeleton1 = pose1[0].skeleton
+      const poses2 = pose2[0].pose
+      const skeleton2 = pose2[0].skeleton
+      for (let i = 0; i < poses2.keypoints.length; i++) {
+        let keypoint1 = poses1.keypoints[i]
+        console.log(keypoint1)
+        let keypoint2 = poses2.keypoints[i]
+        const key_dist = dist(keypoint1.position.x, keypoint1.position.y, keypoint2.position.x, keypoint2.position.y)
+        if(keypoint1.score >  0.1 && keypoint2.score > 0.1 && i > 4){
+          if (key_dist < 20) {
+            noStroke()
+            fill("rgba(12, 236, 221, 0.9)")
+            ellipse(keypoint1.position.x, keypoint1.position.y, 10, 10)
+            textSize(12)
+            text(keypoint1.part, keypoint1.position.x+8, keypoint1.position.y+8)
+          } else {
+            noStroke()
+            fill("rgba(255, 103, 231,0.9)")
+            ellipse(keypoint1.position.x, keypoint1.position.y, 10, 10)
+            textSize(12)
+            text(keypoint1.part, keypoint1.position.x+8, keypoint1.position.y+8)
+          }
+        }
+      }
+    }
+  }
+}
+
+class Button {
+  constructor(i, inImg) {
+    this.i = i
+    this.x = 8+(i)%2*228+i%2*8;
+    this.y = 16+Math.floor(i/2)*228+i%2*8;
+    this.w = 228
+    this.h = 228
+    this.img = inImg;
+  }
+  display() {
+    stroke(0);
+    if (this.over()) {
+      tint(230, 230, 230);
+    } else {
+      noTint();
+    }
+    image(this.img, this.x, this.y, this.w, this.h);
+  }
+  over() {
+    if (mouseX > this.x && mouseX < this.x + this.w && mouseY > this.y && mouseY < this.y + this.h) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+function mouseReleased() {
+  if (temp_i !== undefined) {
+    pose_flag = true
+  }
+}
